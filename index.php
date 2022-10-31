22ca1966d3c878ce63be8202fe6fe727ab08c6d4
Aavik <91154227+4avik@users.noreply.github.com>
2022-10-31 09:51:55 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once('connection.php');
+
+#echo '<ul>';
+
+$stmt = $pdo->query('SELECT * FROM books WHERE is_deleted=0');
+
+
+# while ($row = $stmt->fetch())
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+
+     <nav style="display: flex; justify-content: space-between;">
+          <a href="add_author.php">Lisa autor</a>
+
+          <form action="index.php" method="get">
+               <input type="text" name="search" palceholder="Otsing">
+               <input type="submit" value="Otsi">
+          </form>
+     </nav>
+
+
+     <main>
+          <ul> 
+          <?php while ($book = $stmt->fetch()) { ?>
+               <li>
+                    <a href="book.php?id=<?=$book['id'];?>"><?=$book['title'];?></a>
+               </li>
+          <?php } ?>
+          </ul>
+     </main>
+
+
+</body>
+</html>
+
+
+<!-- 
+
+{
+     echo '<li><a href="book.php?id=' . $row['id'] . '">' . $row['title'] . '</li>';
+}
+
+echo '</ul>';
+-->
