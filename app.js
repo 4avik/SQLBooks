34130383886bfcf146ad6dc5c27c2cb4697a718f
Aavik <91154227+4avik@users.noreply.github.com>
2022-10-31 10:58:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,15 @@ const authorSelect = document.getElementById('author-dd');
 
 authorSelect.addEventListener('change', function () {
     console.log('You selected: ', this.value, this.options[this.selectedIndex].text);
-    console.log(document.querySelector('.author-row'));
+
+    const submitInput = document.querySelector('input[type="submit"]');
+
+    let newAuthorDiv = document.querySelector('.author-row').cloneNode(true);
+    newAuthorDiv.querySelector('.author-name').innerText = this.options[this.selectedIndex].text;
+
+    submitInput.parentNode.insertBefore(newAuthorDiv, submitInput);
+
+    const selectedOption = document.querySelector('select option[value=" ' + this.value + ' "]');
+
+    selectedOption.remove();
 });
