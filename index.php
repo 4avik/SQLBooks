d6cc8df5c3cba521650dc2b61efd2a08ac19e355
Aavik <91154227+4avik@users.noreply.github.com>
2022-11-06 18:22:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $stmt = $pdo->query('SELECT * FROM books WHERE is_deleted=0');
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
+     <title>Raamatupood</title>
 </head>
 <body>
 
